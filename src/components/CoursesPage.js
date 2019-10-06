3809ead4aec876f24381470ebf094379d43105e6
Yura Dzyha <yura_dz@hotmail.com>
2019-10-06 23:07:13 +0300

    Hooks, Component Composition and PropTypes

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,38 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getCourses } from "../api/courseApi";
+import CourseList from './CourseList';
 
-class CoursesPage extends React.Component {
+function CoursesPage() {
+    const [courses, setCourses] = useState([]);
 
-    state = {
-        courses: []
-    }
+    useEffect(() => {
+        getCourses().then(_courses => setCourses(_courses));
+    }, [])
 
-    componentDidMount() {
-        getCourses().then(courses => this.setState({ courses: courses }));
-    }
 
-    render() {
-        return <>
+    return (
+        <>
             <h2>Courses</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author Id</th>
-                        <th>Category</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.courses.map(course =>
-                        <tr key={course.id}>
-                            <td>{course.title}</td>
-                            <td>{course.authorId}</td>
-                            <td>{course.category}</td>
-                        </tr>)}
-                </tbody>
-            </table>
+            <CourseList courses={courses} />
         </>
-    }
+    );
 }
 
 export default CoursesPage;
