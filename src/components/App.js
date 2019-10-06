7193a122f1ea6278fb5b7b16a1e2c01cb194b132
Yura Dzyha <yura_dz@hotmail.com>
2019-10-06 17:51:03 +0300

    Props, State, Lifcycle Methods and Keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
-import Header from './common/Header'
+import Header from './common/Header';
+import CoursesPage from './CoursesPage';
 
 function App() {
 
     function getPage() {
         const route = window.location.pathname;
-        if (route === "/about") {
-            return <AboutPage />
-        }
+        if (route === "/about") return <AboutPage />
+        if (route === "/courses") return <CoursesPage />
         return <HomePage />
     }
 
